26211f7e729c76359e668e9b0bc9c455f8f20b6f
Bryan Cordoba <al21020065@itsa.edu.mx>
2024-12-09 15:01:59 -0600

    Separé la navbar en un archivo independiente

--- index.php
+++ index.php
@@ -12,26 +12,9 @@
     <link rel="stylesheet" href="Stilos/styles.css">
 </head>
 <body>
-    <container class="header">
-    <nav class="navbar navbar-expand-lg ">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </div>
-        </div>
-    </div>
-    </nav>
-    </container>
+<!-- Incluir Navbar -->
+<?php require_once 'navbar.php'; ?>
 
-    
 
     <!-- SweetAlert JS -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script> 
