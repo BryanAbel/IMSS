ac107f6673468edade1548df5df27ec9818e88b2
Bryan Cordoba <al21020065@itsa.edu.mx>
2024-12-09 18:14:36 -0600

    Estructura medico completa2

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,10 @@
 function registrarMedico() {
-    let datos = $('#formularioRegistroMedico').serialize();  // Obtenemos los datos del formulario
+    event.preventDefault();  // Previene que el formulario se envíe de manera tradicional
+
+    let datos = $('#formularioRegistro').serialize();  // Obtenemos los datos del formulario
+
     $.ajax({
-        url: 'CRegistroMedico.php',  // Archivo PHP que procesará el registro
+        url: '/medicos/registro',  // Esta es la ruta que se encargará de llamar al controlador CRegistroMedico
         type: 'POST',
         data: datos,
         success: function(response) {
@@ -25,77 +28,6 @@ function registrarMedico() {
                 icon: 'error',
                 title: 'Error',
                 text: 'No se pudo registrar al médico.'
-            });
-        }
-    });
-}
-
-function cargarTablaMedicos() {
-    $.ajax({
-        url: 'tablaMedicos.php',  // Archivo que devuelve la tabla con los médicos
-        type: 'POST',
-        success: function(respuesta) {
-            document.getElementById("contenidoTablaMedicos").innerHTML = respuesta;
-        }
-    });
-}
-
-function modificarMedico(idMedico) {
-    // Provocamos un clic automático para abrir el modal
-    $("#btnModalModificarMedico").click();
-
-    $.ajax({
-        url: 'formularioModificacionMedico.php',
-        type: 'POST',
-        data: { "idMedico": idMedico },
-        success: function(respuesta) {
-            document.getElementById("resultadoModificacionMedico").innerHTML = respuesta;
-        }
-    });
-}
-
-function ModiMedico() {
-    let datos = $('#formularioModificacionMedico').serialize();
-    $.ajax({
-        url: 'CModificarMedico.php',  // Archivo PHP que procesa la actualización
-        type: 'POST',
-        data: datos,
-        success: function(data) {
-            cargarTablaMedicos();
-            Swal.fire({
-                icon: 'success',
-                title: 'Médico modificado',
-                text: 'El médico ha sido modificado correctamente.'
-            });
-        },
-        error: function() {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'No se pudo modificar el médico.'
-            });
-        }
-    });
-}
-
-function eliminarMedico(idMedico) {
-    $.ajax({
-        url: 'mEliminarMedico.php',
-        type: 'POST',
-        data: { "idMedico": idMedico },
-        success: function(respuesta) {
-            cargarTablaMedicos();
-            Swal.fire({
-                icon: 'success',
-                title: 'Eliminación exitosa',
-                text: 'El médico ha sido eliminado correctamente.'
-            });
-        },
-        error: function() {
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'No se pudo eliminar el médico.'
             });
         }
     });
--- vistas/medicos/crear.php
+++ vistas/medicos/crear.php
@@ -4,62 +4,56 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registros doc</title>
-<!-- Enlace a la hoja de estilos de Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Enlace a la hoja de estilos de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="funciones.js?v=5"></script>
-
 </head>
 <body>
 
-
-
-
     <div class="container">
-    <div class="card shadow p-4 mt-4 mx-auto" style="max-width: 900%;">
-         <h3>REGISTRO DE DOCTORES</h3>
+        <div class="card shadow p-4 mt-4 mx-auto" style="max-width: 900%;">
+            <h3>REGISTRO DE DOCTORES</h3>
             
-         <div class="card shadow p-4 mt-4 mx-1" style="max-width: ;">
-            <form id="formularioRegistro" method="post">
-              
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" id="nombre" name="iNombre" placeholder="Ingresa nombre">                    
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Segundo nombre</label>
-                    <input type="text" class="form-control" id="SegundoNombre" name="iSegundoApellido"  placeholder="Ingresa Segundo Nombre">
-                </div>   
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Apellido Paterno</label>
-                    <input type="text" class="form-control" id="Ap" name="iApellidoPaterno"  placeholder="Ingresa Apellido Paterno">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Apellido Materno</label>
-                    <input type="text" class="form-control" id="Am" name="iApellidoMeaterno"  placeholder="Ingresa Materno">
-                </div>     
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">fecha de nacimiento</label>
-                    <input type="date" class="form-control" id="Naci" name="iNacimiento"  placeholder="Ingresa Fecha de nacimiento">
-                </div>       
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Anos de servicio</label>
-                    <input type="text" class="form-control" id="Anos" name="iAnos"  placeholder="Ingresa Anos de Servicio">
-                </div>  
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Telefono</label>
-                    <input type="text" class="form-control" id="Tel" name="iTel"  placeholder="Ingresa Telefono">
-                </div>           
-            </form>
-            <button type="submit" class="btn btn-primary" onclick="registrarMedico();">Registrar</button>
-            <hr>
-            <div id="contenidoTablaPersonas"></div>
-        </div>
-      </div>   
+            <div class="card shadow p-4 mt-4 mx-1">
+                <form id="formularioRegistro" method="post">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="nombre" name="iNombre" placeholder="Ingresa nombre">                    
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Segundo nombre</label>
+                        <input type="text" class="form-control" id="SegundoNombre" name="iSegundoApellido" placeholder="Ingresa Segundo Nombre">
+                    </div>   
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="Ap" name="iApellidoPaterno" placeholder="Ingresa Apellido Paterno">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Apellido Materno</label>
+                        <input type="text" class="form-control" id="Am" name="iApellidoMeaterno" placeholder="Ingresa Materno">
+                    </div>     
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
+                        <input type="date" class="form-control" id="Naci" name="iNacimiento" placeholder="Ingresa Fecha de nacimiento">
+                    </div>       
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Años de servicio</label>
+                        <input type="text" class="form-control" id="Anos" name="iAnos" placeholder="Ingresa Años de Servicio">
+                    </div>  
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Teléfono</label>
+                        <input type="text" class="form-control" id="Tel" name="iTel" placeholder="Ingresa Teléfono">
+                    </div>           
+                    <button type="button" class="btn btn-primary" onclick="registrarMedico();">Registrar</button>
+                </form>
+                <hr>
+                <div id="contenidoTablaPersonas"></div>
+            </div>
+        </div>   
     </div>
 
-
- <!--Enlace a JS(jQuery) librería de Javascript para poder utilizar AJAX-->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!--Enlace a JS(jQuery) librería de Javascript para poder utilizar AJAX-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!-- Scripts de Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
